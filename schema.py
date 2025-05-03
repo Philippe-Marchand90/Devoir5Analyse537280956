66894f908a645f2e6b0230fa6220cfecb736192c
Philippe Marchand <PHMAR122@ulaval.ca>
2025-05-03 18:20:48 -0400

    save 2

--- schema.py
+++ schema.py
@@ -13,4 +13,13 @@ def schema(sigma, L, f, h, tau, K):
     def F(t, U):
         UN = np.empty_like(U)
         UN[0] = U[0] + mu*(U[1] - 2*U[0] + 0)
-        
+        for j in range(1, n-2):
+            UN[j] = U[j] + mu*(U[j+1] - 2*U[j] + U[j-1])
+            UN[-1] = U[-1] + mu*(0 - 2*U[-1] + U[-2])
+        return (UN - U) / tau
+
+    t0 = 0.0
+    tf = K * tau
+    t_vals, W = euler_explicite(F, t0, U0, tau, tf)
+
+    return W
